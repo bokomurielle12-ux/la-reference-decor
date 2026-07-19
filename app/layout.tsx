2edae164a61b07002608d3fc421ecd259897282a
Murielle Boko <bokomurielle12@gmail.com-github>
2026-07-19 23:31:49 +0100

    premier déploiement

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "LA RÉFÉRENCE COIFFURE ET DÉCOR",
+  description: "Décoration intérieure, extérieure et événementielle au Bénin",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  );
+}
