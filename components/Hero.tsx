309c600ab0decd940e1ff3d261a0eda64d4e1b07
Murielle Boko <bokomurielle12@gmail.com-github>
2026-07-20 00:35:31 +0100

    Correction structure Next.js et ajout composants

---
+++ components/Hero.tsx
@@ -0,0 +1,7 @@
+export default function Hero() {
+  return (
+    <section>
+      <h1>LA REFERENCE COIFFURE ET DECOR</h1>
+    </section>
+  );
+}
---
+++ components/services.tsx
@@ -0,0 +1,7 @@
+export default function Services() {
+  return (
+    <section>
+      <h2>Nos services</h2>
+    </section>
+  );
+}
